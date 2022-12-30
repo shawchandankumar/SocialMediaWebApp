b8aa0718caff9305268f97b6252de557705a7dea
shawchandankumar20 <shawchandankumar20@gmail.com>
2022-12-30 22:09:40 +0530

    First commit: Setup the directories and express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.error(`Error on running server on port ${port}`);
+        return;
+    }
+
+    console.log(`Server is up and running on port ${port}`);
+})
