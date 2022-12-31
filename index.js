ce329e0a00dffd806efa0c79435a3f77cbb30df8
shawchandankumar20 <shawchandankumar20@gmail.com>
2022-12-31 17:03:56 +0530

    Setup partials and layouts and static files for layouts and pages

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+
+// include all the css js and images file
+app.use(express.static('./assets'));
+
+// use express layouts before router
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // use express router
 app.use('/', require('./routes'));
